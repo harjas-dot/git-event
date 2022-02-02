9c1600a8d92c9dc59d282ee9afbfed649eea6395
Harjaspreet singh <68768529+harjas-dot@users.noreply.github.com>
2022-02-02 22:56:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let imgArray = ["segway.jpg","jfkmes.jpg","backcolor.jpg","Reunto.jpg", "segway.jpg","jfkmes.jpg","backcolor.jpg","Reunto.jpg"];
+let imgArray = ["images/chabeel.png",
+"images/orientation.jpeg",
+"images/IMG_1493.JPG",
+"images/IMG_2273.JPG",
+"images/turban.png",
+"images/IMG_2541.JPG",
+"images/IMG_2710.JPG",
+];
 let contentArray=["To quench the thirst that exists given the scorching heat, Chabeel is organised. Free sweet, cool, non-alcoholic drinks are offered by remembering the martyrdom of Guru Arjan Dev Ji.Guru taught the sikhs to accept God’s will as sweet and aimed at raising people's spirits and instilling a feeling of eternal optimism.",
   "Orientation is a terse introduction about the society- VIRSA. Presentations and performances about the society are delivered to welcome the new students. The Virsa team interacts with the students and have fun.",
   "Experiences speak and so do the Eminent Personalities who grace this Event and offer an insight into their lives. The purpose of conducting this Talk Show is to advice Students to Welcome their forthcomings and Provide them with Motivation and a Positive Approach.",
@@ -10,7 +17,9 @@ let headingArray=["Chabeel",
 "Orientation",
 "Virsa Talks",
 "Haye-o-Rabba",
-"Turban Tying","Informals","Jaggo"
+"Turban Tying",
+"Informals",
+"Jaggo"
 ]
 let NextBtn = document.querySelector(".Next");
 let PrevBtn = document.querySelector(".Prev");
@@ -18,27 +27,60 @@ let PrevBtn = document.querySelector(".Prev");
 let n = 0; //Image Counter
 
 function pictureChangeNext(){
-    if(n>=0 && n<5){
-        document.querySelector(".leftImg").src=imgArray[n+1];
-        document.querySelector(".centerImg").src=imgArray[n+2];
-        document.querySelector(".rightImg").src=imgArray[n+3];
-        document.querySelector(".content").innerHTML=contentArray[n+2];
-        document.querySelector(".name").innerHTML=headingArray[n+2];
-        n = n+1;
+    if(n>=0 || n<=6 ){
+      n=n+1;
+      if(n==7){
+        n=0;
+        document.querySelector(".leftImg").src=imgArray[6];
+        document.querySelector(".centerImg").src=imgArray[n];
+        document.querySelector(".rightImg").src=imgArray[n+1];
+        document.querySelector(".content").innerHTML=contentArray[n];
+        document.querySelector(".name").innerHTML=headingArray[n];
+      }
+      else{
+
+        document.querySelector(".leftImg").src=imgArray[n-1];
+        document.querySelector(".centerImg").src=imgArray[n];
+        if(n==6){
+          document.querySelector(".rightImg").src=imgArray[0];
+        }
+        else{
+          document.querySelector(".rightImg").src=imgArray[n+1];
+        }
+
+        document.querySelector(".content").innerHTML=contentArray[n];
+        document.querySelector(".name").innerHTML=headingArray[n];
+
+      }
         console.log(n);
     }
 }
 
 function pictureChangePrev(){
 
-    if(n>0 && n<=5){
-        n = n-1;
-        document.querySelector(".leftImg").src=imgArray[n];
-        document.querySelector(".centerImg").src=imgArray[n+1];
-        document.querySelector(".rightImg").src=imgArray[n+2];
-        document.querySelector(".content").innerHTML=contentArray[n+1];
-        document.querySelector(".name").innerHTML=headingArray[n+1];
+    if(n>=0 || n<=6){
 
+      if(n==0){
+        n=6;
+        document.querySelector(".leftImg").src=imgArray[n-1];
+        document.querySelector(".centerImg").src=imgArray[n];
+        document.querySelector(".rightImg").src=imgArray[0];
+        document.querySelector(".content").innerHTML=contentArray[n];
+        document.querySelector(".name").innerHTML=headingArray[n];
+      }
+      else{
+        n = n-1;
+        if(n==0){
+          document.querySelector(".leftImg").src=imgArray[6];
+        }
+        else{
+          document.querySelector(".leftImg").src=imgArray[n-1];
+        }
+        document.querySelector(".centerImg").src=imgArray[n];
+        document.querySelector(".rightImg").src=imgArray[n+1];
+        document.querySelector(".content").innerHTML=contentArray[n];
+        document.querySelector(".name").innerHTML=headingArray[n];
+      }
         console.log(n)
     }
 }
